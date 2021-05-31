df7aba0a9ef45f36851d6ca2611db34f5ffe249e
chaerulmarwan20 <chaerulmarwan20@gmail.com>
2021-05-31 23:10:31 +0700

    add cron job

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const host = process.env.HOST;
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const moment = require("moment");
+const cron = require("node-cron");
 const path = require("path");
 
 // Router
@@ -15,6 +17,9 @@ const router = require("./app/routers");
 
 // Express
 const app = express();
+
+// Model
+const scheduleModel = require("./app/models/scheduleModel");
 
 app.use("/images", express.static(path.join(__dirname, "images")));
 app.use(express.json());
@@ -55,4 +60,81 @@ app.use((err, req, res, next) => {
 
 app.listen(port, () => {
   console.log(`Server is running on http://${host}:${port}`);
+  cron.schedule("0 3 * * *", () => {
+    const dataMovie = [
+      {
+        id: 86,
+        title: "1917",
+        category: "13+",
+      },
+      {
+        id: 88,
+        title: "Bad Boys For Life",
+        category: "R",
+      },
+      {
+        id: 90,
+        title: "Extraction",
+        category: "R",
+      },
+      {
+        id: 91,
+        title: "Ford v Ferrari",
+        category: "13+",
+      },
+      {
+        id: 92,
+        title: "Godzilla vs. Kong",
+        category: "PG-13",
+      },
+    ];
+    const cinema = [1, 2, 3];
+    const city = [1, 2, 3];
+    const time = [
+      "10:00am",
+      "01:00pm",
+      "03:00pm",
+      "05:00pm",
+      "07:00pm",
+      "09:00pm",
+      "11:00pm",
+    ];
+    dataMovie.map(async (item, index) => {
+      for (let c = 0; c < city.length; c++) {
+        for (let a = 0; a < cinema.length; a++) {
+          const date = new Date();
+          const dayNow = moment(date).format("dddd");
+          const dataSchedule = {
+            day: dayNow,
+            date,
+            price: 30000,
+            time: JSON.stringify(time),
+            idCity: city[c],
+            idMovie: item.id,
+            idCinema: cinema[a],
+          };
+          let idSchedule;
+          const scheduleResult = await scheduleModel.createSchedule(
+            dataSchedule
+          );
+          idSchedule = scheduleResult[0].id;
+          for (let i = 0; i < time.length; i++) {
+            for (let j = 1; j <= 98; j++) {
+              const dataTicket = {
+                movieTitle: item.title,
+                category: item.category,
+                available: true,
+                idSchedule,
+                time: time[i],
+                idSeat: j,
+                idMovie: item.id,
+              };
+              await scheduleModel.createTicket(dataTicket);
+            }
+          }
+        }
+      }
+      console.log(`${index} Schedule and ticket has been created`);
+    });
+  });
 });
